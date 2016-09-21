3d0c23a3a2e35a888e8d35c883b7db9e346f6bdf
Boris94cr <baach94@hotmail.com>
2016-09-21 10:47:10 -0600

    versión 1 lab 3

--- Lab3/codigo/src/main.c
+++ Lab3/codigo/src/main.c
@@ -1,30 +1,36 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2010 Gareth McMullin <gareth@blacksphere.co.nz>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
+#include <stdlib.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/usb/usbd.h>
+#include <libopencm3/usb/cdc.h>
+#include <libopencm3/cm3/scb.h>
 #include <libopencm3/stm32/timer.h>
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/stm32/exti.h>
 #include <libopencmsis/core_cm3.h>
 
-uint16_t frequency_sequence[18] = {
-	15000,// primeros 10 segundos del ciclo de espera, en los cuales se acepta solicitud
-	750,//toggle del led3 indicando que se apagara y dara paso a los peatones
-	750,
-	750,
-	750,
-	750,
-	750,
-	1500,//amarillo
-	1500,//rojo
-	15000,//peatones tienen 10 segundos para pasar
-	750,//toggle del led4 indicando que se apagara y dara paso a los vehiculos
-	750,
-	750,
-	750,
-	750,
-	750,
-	1500,
-	750
+uint16_t frequency_sequence[2] = {
+	1000,
+	1000
 };
 
 uint16_t frequency_sel;
@@ -32,9 +38,150 @@ uint16_t frequency_sel;
 uint16_t compare_time;
 uint16_t new_time;
 uint16_t frequency;
-bool solicitud;
-bool fin_ciclo;
-bool seg10; 
+char buffer[3]={'a', 'b', 'c'};
+
+static const struct usb_device_descriptor dev = {
+	.bLength = USB_DT_DEVICE_SIZE,
+	.bDescriptorType = USB_DT_DEVICE,
+	.bcdUSB = 0x0200,
+	.bDeviceClass = USB_CLASS_CDC,
+	.bDeviceSubClass = 0,
+	.bDeviceProtocol = 0,
+	.bMaxPacketSize0 = 64,
+	.idVendor = 0x0483,
+	.idProduct = 0x5740,
+	.bcdDevice = 0x0200,
+	.iManufacturer = 1,
+	.iProduct = 2,
+	.iSerialNumber = 3,
+	.bNumConfigurations = 1,
+};
+
+/*
+ * This notification endpoint isn't implemented. According to CDC spec it's
+ * optional, but its absence causes a NULL pointer dereference in the
+ * Linux cdc_acm driver.
+ */
+static const struct usb_endpoint_descriptor comm_endp[] = {{
+	.bLength = USB_DT_ENDPOINT_SIZE,
+	.bDescriptorType = USB_DT_ENDPOINT,
+	.bEndpointAddress = 0x83,
+	.bmAttributes = USB_ENDPOINT_ATTR_INTERRUPT,
+	.wMaxPacketSize = 16,
+	.bInterval = 255,
+} };
+
+static const struct usb_endpoint_descriptor data_endp[] = {{
+	.bLength = USB_DT_ENDPOINT_SIZE,
+	.bDescriptorType = USB_DT_ENDPOINT,
+	.bEndpointAddress = 0x01,
+	.bmAttributes = USB_ENDPOINT_ATTR_BULK,
+	.wMaxPacketSize = 64,
+	.bInterval = 1,
+}, {
+	.bLength = USB_DT_ENDPOINT_SIZE,
+	.bDescriptorType = USB_DT_ENDPOINT,
+	.bEndpointAddress = 0x82,
+	.bmAttributes = USB_ENDPOINT_ATTR_BULK,
+	.wMaxPacketSize = 64,
+	.bInterval = 1,
+} };
+
+static const struct {
+	struct usb_cdc_header_descriptor header;
+	struct usb_cdc_call_management_descriptor call_mgmt;
+	struct usb_cdc_acm_descriptor acm;
+	struct usb_cdc_union_descriptor cdc_union;
+} __attribute__((packed)) cdcacm_functional_descriptors = {
+	.header = {
+		.bFunctionLength = sizeof(struct usb_cdc_header_descriptor),
+		.bDescriptorType = CS_INTERFACE,
+		.bDescriptorSubtype = USB_CDC_TYPE_HEADER,
+		.bcdCDC = 0x0110,
+	},
+	.call_mgmt = {
+		.bFunctionLength =
+			sizeof(struct usb_cdc_call_management_descriptor),
+		.bDescriptorType = CS_INTERFACE,
+		.bDescriptorSubtype = USB_CDC_TYPE_CALL_MANAGEMENT,
+		.bmCapabilities = 0,
+		.bDataInterface = 1,
+	},
+	.acm = {
+		.bFunctionLength = sizeof(struct usb_cdc_acm_descriptor),
+		.bDescriptorType = CS_INTERFACE,
+		.bDescriptorSubtype = USB_CDC_TYPE_ACM,
+		.bmCapabilities = 0,
+	},
+	.cdc_union = {
+		.bFunctionLength = sizeof(struct usb_cdc_union_descriptor),
+		.bDescriptorType = CS_INTERFACE,
+		.bDescriptorSubtype = USB_CDC_TYPE_UNION,
+		.bControlInterface = 0,
+		.bSubordinateInterface0 = 1,
+	 }
+};
+
+static const struct usb_interface_descriptor comm_iface[] = {{
+	.bLength = USB_DT_INTERFACE_SIZE,
+	.bDescriptorType = USB_DT_INTERFACE,
+	.bInterfaceNumber = 0,
+	.bAlternateSetting = 0,
+	.bNumEndpoints = 1,
+	.bInterfaceClass = USB_CLASS_CDC,
+	.bInterfaceSubClass = USB_CDC_SUBCLASS_ACM,
+	.bInterfaceProtocol = USB_CDC_PROTOCOL_AT,
+	.iInterface = 0,
+
+	.endpoint = comm_endp,
+
+	.extra = &cdcacm_functional_descriptors,
+	.extralen = sizeof(cdcacm_functional_descriptors)
+} };
+
+static const struct usb_interface_descriptor data_iface[] = {{
+	.bLength = USB_DT_INTERFACE_SIZE,
+	.bDescriptorType = USB_DT_INTERFACE,
+	.bInterfaceNumber = 1,
+	.bAlternateSetting = 0,
+	.bNumEndpoints = 2,
+	.bInterfaceClass = USB_CLASS_DATA,
+	.bInterfaceSubClass = 0,
+	.bInterfaceProtocol = 0,
+	.iInterface = 0,
+
+	.endpoint = data_endp,
+} };
+
+static const struct usb_interface ifaces[] = {{
+	.num_altsetting = 1,
+	.altsetting = comm_iface,
+}, {
+	.num_altsetting = 1,
+	.altsetting = data_iface,
+} };
+
+static const struct usb_config_descriptor config = {
+	.bLength = USB_DT_CONFIGURATION_SIZE,
+	.bDescriptorType = USB_DT_CONFIGURATION,
+	.wTotalLength = 0,
+	.bNumInterfaces = 2,
+	.bConfigurationValue = 1,
+	.iConfiguration = 0,
+	.bmAttributes = 0x80,
+	.bMaxPower = 0x32,
+
+	.interface = ifaces,
+};
+
+static const char * usb_strings[] = {
+	"Black Sphere Technologies",
+	"CDC-ACM Demo",
+	"DEMO",
+};
+
+/* Buffer to be used for control requests. */
+uint8_t usbd_control_buffer[128];
 
 static void clock_setup(void)
 {
@@ -45,17 +192,28 @@ static void clock_setup(void)
 static void gpio_setup(void)
 {
 
-	/* Enable GPIO clock for leds. */
+	// Habilita el reloj 
 	rcc_periph_clock_enable(RCC_GPIOD);
+    	rcc_periph_clock_enable(RCC_GPIOA);
 
-	/* Set GPIO12 (in GPIO port D) to 'output push-pull'. */
-	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT,
-		      GPIO_PUPD_NONE, GPIO1 | GPIO2 | GPIO3 | GPIO4 | GPIO5 | GPIO12 | GPIO13 | GPIO14 | GPIO15);
-
-   	rcc_periph_clock_enable(RCC_GPIOA);
-	gpio_mode_setup(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO0); 
+   	 // Configura los pines como entradas USADOS POR EL ADC
+	gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO0);
+   	gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO1);
 
 }
+
+/*static void adc_setup(void)
+{
+	//habilitar el reloj en el periferico
+	rcc_periph_clock_enable(RCC_ADC1);
+
+	//configurar el convertidor A/D
+	adc_power_off(ADC1);
+	adc_disable_scan_mode(ADC1);
+	adc_set_sample_time_on_all_channels(ADC1, ADC_SMPR_SMP_3CYC);
+	adc_power_on(ADC1);
+}*/
+
 
 static void tim_setup(void)
 {
@@ -76,27 +234,7 @@ static void tim_setup(void)
 	timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT,
 		       TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
 
-	/* Reset prescaler value.
-	 * Running the clock at 5kHz.
-	 */
-	/*
-	 * On STM32F4 the timers are not running directly from pure APB1 or
-	 * APB2 clock busses.  The APB1 and APB2 clocks used for timers might
-	 * be the double of the APB1 and APB2 clocks.  This depends on the
-	 * setting in DCKCFGR register. By default the behaviour is the
-	 * following: If the Prescaler APBx is greater than 1 the derived timer
-	 * APBx clocks will be double of the original APBx frequencies. Only if
-	 * the APBx prescaler is set to 1 the derived timer APBx will equal the
-	 * original APBx frequencies.
-	 *
-	 * In our case here the APB1 is devided by 4 system frequency and APB2
-	 * divided by 2. This means APB1 timer will be 2 x APB1 and APB2 will
-	 * be 2 x APB2. So when we try to calculate the prescaler value we have
-	 * to use rcc_apb1_freqency * 2!!! 
-	 *
-	 * For additional information see reference manual for the stm32f4
-	 * familiy of chips. Page 204 and 213
-	 */
+	
 	timer_set_prescaler(TIM2, ((rcc_apb1_frequency * 2) / 10));
 
 	/* Enable preload. */
@@ -144,101 +282,100 @@ void tim2_isr(void)
 		/* Clear compare interrupt flag. */
 		timer_clear_flag(TIM2, TIM_SR_CC1IF);
 		/* activación de las luces del semaforo*/
-		if(solicitud){
-			seg10 = false;
-			fin_ciclo = false; //rechazamos solicitudes de paso
-			/*if(frequency_sel == 0){
-				10seg = true;	
-				gpio_set(GPIOD, GPIO12 | GPIO15); //paso Vehiculos/alto Peatones
-				gpio_clear(GPIOD, GPIO13 | GPIO14);
-			}*/
-			if((frequency_sel >= 1)&&(frequency_sel <= 6)){
-				gpio_toggle(GPIOD, GPIO1);//alerta vehiculos/cambio a roja
+		
+		//usbd_ep_write_packet(usbd_dev, 0x82, buffer, 3);
 
-			}
-			else if(frequency_sel == 7){
-				gpio_clear(GPIOD, GPIO1);
-				gpio_set(GPIOD, GPIO2);//amarillo
-			}
-			else if(frequency_sel == 8){
-				gpio_clear(GPIOD, GPIO2);
-				gpio_set(GPIOD, GPIO3);//alto vehiculos
-			}
-			else if(frequency_sel == 9){
-				gpio_set(GPIOD, GPIO4);//luz verde para peatones
-				gpio_clear(GPIOD, GPIO1 | GPIO5);
-			}
-			else if((frequency_sel > 9)&&(frequency_sel <= 15)){
-				gpio_toggle(GPIOD, GPIO4);//alerta peatones/cambio a roja
-			}
-			else if(frequency_sel == 16){
-				gpio_clear(GPIOD, GPIO4);//espera de un segundo antes de dar
-				gpio_set(GPIOD, GPIO5);//luz verde a vehiculos
-			}
-			else{
-				gpio_set(GPIOD, GPIO1 | GPIO5);//paso a vehiculos
-				gpio_clear(GPIOD, GPIO3 | GPIO4);
-				fin_ciclo = true;
-				solicitud = false;	
-			}
-	
-			compare_time = timer_get_counter(TIM2);
-			frequency = frequency_sequence[frequency_sel++];
-			new_time = compare_time + frequency;
-			timer_set_oc_value(TIM2, TIM_OC1, new_time);				
-		}
-		else{
-			gpio_set(GPIOD, GPIO1 | GPIO5);//paso a vehiculos
-			gpio_clear(GPIOD, GPIO4 | GPIO3);
+		compare_time = timer_get_counter(TIM2);
+		frequency = frequency_sequence[frequency_sel++];
+		new_time = compare_time + frequency;
+		timer_set_oc_value(TIM2, TIM_OC1, new_time);
 			
-			if(seg10) {
-	
-				compare_time = timer_get_counter(TIM2);
-				new_time = compare_time + 500;
-				timer_set_oc_value(TIM2, TIM_OC1, new_time);
-				gpio_set(GPIOD, GPIO13);
-				frequency_sel = 1;
-			}
-			
-			else {
-				compare_time = timer_get_counter(TIM2);
-				frequency = frequency_sequence[frequency_sel++];
-				new_time = compare_time + frequency;
-				timer_set_oc_value(TIM2, TIM_OC1, new_time);
-				seg10 = true;			
-			}
-		}
-
-		if(frequency_sel == 18){
+		if(frequency_sel == 1){
 			frequency_sel = 0;
 		}
 	}
 }
 
-int main(void)
-{	
-	clock_setup();
-	gpio_setup();
-	tim_setup();
-	solicitud = false;
-	fin_ciclo = true;
-	seg10 = false;
-	frequency_sel = 0;
+static int cdcacm_control_request(usbd_device *usbd_dev,
+	struct usb_setup_data *req, uint8_t **buf, uint16_t *len,
+	void (**complete)(usbd_device *usbd_dev, struct usb_setup_data *req))
+{
+	(void)complete;
+	(void)buf;
+	(void)usbd_dev;
 
-
-	/* Loop calling Wait For Interrupt. In older pre cortex ARM this is
-	 * just equivalent to nop. On cortex it puts the cpu to sleep until
-	 * one of the three occurs:
-	 *
-	 * a non-masked interrupt occurs and is taken
-	 * an interrupt masked by PRIMASK becomes pending
-	 * a Debug Entry request
-	 */
-	while (1){
-		if(gpio_get(GPIOA, GPIO0) && fin_ciclo){
-			solicitud = true;
+	switch (req->bRequest) {
+	case USB_CDC_REQ_SET_CONTROL_LINE_STATE: {
+		/*
+		 * This Linux cdc_acm driver requires this to be implemented
+		 * even though it's optional in the CDC spec, and we don't
+		 * advertise it in the ACM functional descriptor.
+		 */
+		return 1;
 		}
-		//__WFI(); /* Wait For Interrupt. */
+	case USB_CDC_REQ_SET_LINE_CODING:
+		if (*len < sizeof(struct usb_cdc_line_coding)) {
+			return 0;
+		}
+
+		return 1;
 	}
 	return 0;
+}
+
+static void cdcacm_data_rx_cb(usbd_device *usbd_dev, uint8_t ep)
+{
+	(void)ep;
+
+	char buf[64];
+	int len = usbd_ep_read_packet(usbd_dev, 0x01, buf, 64);
+
+	if (len) {
+		while (usbd_ep_write_packet(usbd_dev, 0x82, buf, len) == 0);
+	}
+}
+
+static void cdcacm_set_config(usbd_device *usbd_dev, uint16_t wValue)
+{
+	(void)wValue;
+
+	/*usbd_ep_setup(usbd_dev, 0x01, USB_ENDPOINT_ATTR_BULK, 64,
+			cdcacm_data_rx_cb);*/
+	usbd_ep_setup(usbd_dev, 0x82, USB_ENDPOINT_ATTR_BULK, 64, NULL);
+	usbd_ep_setup(usbd_dev, 0x83, USB_ENDPOINT_ATTR_INTERRUPT, 16, NULL);
+
+	usbd_register_control_callback(
+				usbd_dev,
+				USB_REQ_TYPE_CLASS | USB_REQ_TYPE_INTERFACE,
+				USB_REQ_TYPE_TYPE | USB_REQ_TYPE_RECIPIENT,
+				cdcacm_control_request);
+}
+
+int main(void)
+{
+	clock_setup();
+	gpio_setup();
+	//adc_setup();
+	tim_setup();
+
+	usbd_device *usbd_dev;
+
+	
+
+	/*rcc_periph_clock_enable(RCC_GPIOA);*/
+	rcc_periph_clock_enable(RCC_OTGFS);
+
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE,
+			GPIO9 | GPIO11 | GPIO12);
+	gpio_set_af(GPIOA, GPIO_AF10, GPIO9 | GPIO11 | GPIO12);
+
+	usbd_dev = usbd_init(&otgfs_usb_driver, &dev, &config,
+			usb_strings, 3,
+			usbd_control_buffer, sizeof(usbd_control_buffer));
+
+	usbd_register_set_config_callback(usbd_dev, cdcacm_set_config);
+
+	while (1) {
+		usbd_poll(usbd_dev);
+	}
 }
