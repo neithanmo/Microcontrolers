faa7b7153061aec381c10a428aa2e5504e2c3829
neithanmo <neithanmo@gmail.com>
2016-09-30 19:00:53 -0600

    otro programa para graficar usando QT

---
+++ Lab3/codigo/src/plot/mainwindow.cpp
@@ -0,0 +1,212 @@
+
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+#include <QDesktopWidget>
+#include <QScreen>
+#include <QMessageBox>
+#include <QMetaEnum>
+
+MainWindow::MainWindow(QWidget *parent) :
+  QMainWindow(parent),
+  ui(new Ui::MainWindow)
+{
+  ui->setupUi(this);
+  setGeometry(400, 250, 542, 390);
+  setupRealtimeDataDemo(ui->customPlot);
+  setWindowTitle("Microcontroladores: Lab.3");
+  statusBar()->clearMessage();
+  ui->customPlot->replot();
+
+}
+
+
+
+void MainWindow::setupRealtimeDataDemo(QCustomPlot *customPlot)
+{
+  demoName = "Real Time Data Demo";
+  
+  // include this section to fully disable antialiasing for higher performance:
+  /*customPlot->setNotAntialiasedElements(QCP::aeAll);
+  QFont font;
+  font.setStyleStrategy(QFont::NoAntialias);
+  customPlot->xAxis->setTickLabelFont(font);
+  customPlot->yAxis->setTickLabelFont(font);
+  customPlot->legend->setFont(font);*/
+
+  serial_fd = serial_open("/dev/ttyACM0",115200);
+  if (serial_fd == -1) {
+          printf ("Error opening the serial device:\n");
+          perror("OPEN");
+          exit(-1);
+  }
+  customPlot->addGraph(); // blue line
+  customPlot->graph(0)->setPen(QPen(QColor(40, 110, 255)));
+  customPlot->addGraph(); // red line
+  customPlot->graph(1)->setPen(QPen(QColor(255, 110, 40)));
+
+  QSharedPointer<QCPAxisTickerTime> timeTicker(new QCPAxisTickerTime);
+  timeTicker->setTimeFormat("%h:%m:%s");
+  customPlot->xAxis->setTicker(timeTicker);
+  customPlot->axisRect()->setupFullAxesBox();
+  customPlot->yAxis->setRange(-1.2, 1.2);
+  customPlot->
+  
+  // make left and bottom axes transfer their ranges to right and top axes:
+  connect(customPlot->xAxis, SIGNAL(rangeChanged(QCPRange)), customPlot->xAxis2, SLOT(setRange(QCPRange)));
+
+  
+  // setup a timer that repeatedly calls MainWindow::realtimeDataSlot:
+  connect(&dataTimer, SIGNAL(timeout()), this, SLOT(realtimeDataSlot()));
+  dataTimer.start(0); // Interval 0 means to refresh as fast as possible
+}
+
+int MainWindow::serial_open(char *serial_name, speed_t baud)
+{
+      struct termios newtermios;
+      int fd;
+      fd = open(serial_name,O_RDWR | O_NOCTTY);
+      newtermios.c_cflag= CBAUD | CS8 | CLOCAL | CREAD;
+      newtermios.c_iflag=IGNPAR;
+      newtermios.c_oflag=0;
+      newtermios.c_lflag=0;
+      newtermios.c_cc[VMIN]=1;
+      newtermios.c_cc[VTIME]=0;
+      cfsetospeed(&newtermios,baud);
+      cfsetispeed(&newtermios,baud);
+      if (tcflush(fd,TCIFLUSH)==-1) return -1;
+      if (tcflush(fd,TCOFLUSH)==-1) return -1;
+      if (tcsetattr(fd,TCSANOW,&newtermios)==-1) return -1;
+      return fd;
+}
+
+
+
+void MainWindow::realtimeDataSlot()
+{
+  int n;
+
+  static QTime time(QTime::currentTime());
+  // calculate two new data points:
+  double key = time.elapsed()/300.0; // time elapsed since start of demo, in seconds
+  static double lastPointKey = 0;
+  if (key-lastPointKey > 0.00001) // at most add point every 2 ms
+  {
+    // add data to lines:
+    n=read (serial_fd, &data, sizeof(short int));//lee linea a linea el caracter de 16 bits
+    y = (data-2047)>0 ? (data-2047)/2048.00 : (data-2047)/2047.00;
+    ui->customPlot->graph(0)->addData(key, y);
+    //ui->customPlot->graph(1)->addData(key, qCos(key)+qrand()/(double)RAND_MAX*0.5*qSin(key/0.4364));
+    // rescale value (vertical) axis to fit the current data:
+    //ui->customPlot->graph(0)->rescaleValueAxis();
+    //ui->customPlot->graph(1)->rescaleValueAxis(true);
+    lastPointKey = key;
+  }
+  // make key axis range scroll with the data (at a constant range size of 8):
+  ui->customPlot->xAxis->setRange(key, 8, Qt::AlignRight);
+  ui->customPlot->replot();
+  
+  // calculate frames per second:
+  static double lastFpsKey;
+  static int frameCount;
+  ++frameCount;
+  if (key-lastFpsKey > 2) // average fps over 2 seconds
+  {
+    ui->statusBar->showMessage(
+          QString("%1 FPS, Total Data points: %2")
+          .arg(frameCount/(key-lastFpsKey), 0, 'f', 0)
+          .arg(ui->customPlot->graph(0)->data()->size()+ui->customPlot->graph(1)->data()->size())
+          , 0);
+    lastFpsKey = key;
+    frameCount = 0;
+  }
+}
+
+void MainWindow::bracketDataSlot()
+{
+  double secs = QCPAxisTickerDateTime::dateTimeToKey(QDateTime::currentDateTime());
+  
+  // update data to make phase move:
+  int n = 500;
+  double phase = secs*5;
+  double k = 3;
+  QVector<double> x(n), y(n);
+  for (int i=0; i<n; ++i)
+  {
+    x[i] = i/(double)(n-1)*34 - 17;
+    y[i] = qExp(-x[i]*x[i]/20.0)*qSin(k*x[i]+phase);
+  }
+  ui->customPlot->graph()->setData(x, y);
+  
+  itemDemoPhaseTracer->setGraphKey((8*M_PI+fmod(M_PI*1.5-phase, 6*M_PI))/k);
+  
+  ui->customPlot->replot();
+  
+  // calculate frames per second:
+  double key = secs;
+  static double lastFpsKey;
+  static int frameCount;
+  ++frameCount;
+  if (key-lastFpsKey > 2) // average fps over 2 seconds
+  {
+    ui->statusBar->showMessage(
+          QString("%1 FPS, Total Data points: %2")
+          .arg(frameCount/(key-lastFpsKey), 0, 'f', 0)
+          .arg(ui->customPlot->graph(0)->data()->size())
+          , 0);
+    lastFpsKey = key;
+    frameCount = 0;
+  }
+}
+
+void MainWindow::setupPlayground(QCustomPlot *customPlot)
+{
+  Q_UNUSED(customPlot)
+}
+
+MainWindow::~MainWindow()
+{
+  delete ui;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
